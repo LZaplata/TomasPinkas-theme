328f77f66d457f52d4d6d34f177ec75ce5317e5f
Lukáš Záplata <lukas.zaplata@gmail.com>
2022-12-07 12:09:50 +0100

    theme initialization

--- assets/src/js/theme.js
+++ assets/src/js/theme.js
@@ -8,45 +8,45 @@ window.lgZoom = lgThumbnail;
 
 window.$ = window.jQuery = require("jquery");
 
-lightGallery(document.getElementById("klienti"), {
-    selector: ".image",
-    plugins: [lgZoom, lgThumbnail],
-    speed: 500,
-});
+// lightGallery(document.getElementById("klienti"), {
+//     selector: ".image",
+//     plugins: [lgZoom, lgThumbnail],
+//     speed: 500,
+// });
 
-var swiper = new Swiper(".reviews-swiper", {
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-});
+// var swiper = new Swiper(".reviews-swiper", {
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//     },
+// });
 
-var swiperInstagram = new Swiper(".instagram-swiper", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true,
-    breakpoints: {
-        576: {
-            slidesPerView: 3,
-        },
-        768: {
-            slidesPerView: 4,
-        },
-        1200: {
-            slidesPerView: 5,
-        },
-        1400: {
-            slidesPerView: 6,
-        },
-        2000: {
-            slidesPerView: 9,
-        },
-    },
-});
+// var swiperInstagram = new Swiper(".instagram-swiper", {
+//     slidesPerView: 2,
+//     spaceBetween: 30,
+//     centeredSlides: true,
+//     loop: true,
+//     breakpoints: {
+//         576: {
+//             slidesPerView: 3,
+//         },
+//         768: {
+//             slidesPerView: 4,
+//         },
+//         1200: {
+//             slidesPerView: 5,
+//         },
+//         1400: {
+//             slidesPerView: 6,
+//         },
+//         2000: {
+//             slidesPerView: 9,
+//         },
+//     },
+// });
 
 $(document).ready(function () {
     $("#offcanvas .nav li a").click(function (event) {
